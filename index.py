b6b2d268f5790b6d3d9089d0f801d26dcc4b7b93
HaseebImd <haseeb.imdad@yahoo.com>
2024-10-12 20:31:31 +0500

    Port Variable Added

--- index.py
+++ index.py
@@ -2,9 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import numpy as np
+import os
 
 # Initialize the Flask app
 app = Flask(__name__)
+
+port = int(os.environ.get("PORT", 5000))
 
 # Enable CORS for all routes and all origins
 CORS(app)
@@ -64,4 +67,4 @@ def predict():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
